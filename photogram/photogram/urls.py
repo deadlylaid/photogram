bd7dd26ff961373b9b9fa8c204b9b499f5bc4c21
minsoo <deadlylaid@gmail.com>
2016-03-28 17:57:53 +0000

    update: urls.py add home

--- photogram/photogram/urls.py
+++ photogram/photogram/urls.py
@@ -1,8 +1,11 @@
 from django.conf.urls import url
 from django.contrib import admin
 from users.views import *
+from photogram.views import *
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^login/$', LoginView.as_view()),
+
+    url(r'^$', HomeView.as_view(), name='home'),
 ]
