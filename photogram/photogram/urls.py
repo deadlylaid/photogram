6b587660cf0cf5d8bf6ba82cc8389932d35906a3
minsoo <deadlylaid@gmail.com>
2016-03-28 18:18:42 +0000

    update: logout create logout view

--- photogram/photogram/urls.py
+++ photogram/photogram/urls.py
@@ -5,7 +5,8 @@ from photogram.views import *
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
-    url(r'^login/$', LoginView.as_view()),
+    url(r'^login/$', LoginView.as_view(), name='login'),
+    url(r'^logout/$', LogoutView.as_view(), name='logout'),
 
     url(r'^$', HomeView.as_view(), name='home'),
 ]
