0d4f3feae76acbb0bdeacf0a717762a171ce2635
minsoo <deadlylaid@gmail.com>
2016-03-29 05:11:30 +0000

    update : urls.py

--- photogram/photogram/urls.py
+++ photogram/photogram/urls.py
@@ -13,8 +13,12 @@ urlpatterns = [
     url(r'^login/$', LoginView.as_view(), name='login'),
     url(r'^logout/$', LogoutView.as_view(), name='logout'),
     url(r'^signup/$', SignupView.as_view(), name='signup'),
-    url(r'^(?P<slug>\w+)/$', ProfileView.as_view(), name='profile'),
+
+
     url(r'^posts/$', PostListView.as_view(), name="list"),
     url(r'^posts/new/$', PostCreateView.as_view(), name="new-post"),
     url(r'^posts/(?P<pk>\d+)/$', PostDetailView.as_view(), name="post"),
-]
+    url(r'^posts/(?P<pk>\d+)/comments/$', PostCommentCreateView.as_view(), name="comment"),
+
+    url(r'^(?P<slug>\w+)/$', ProfileView.as_view(), name='profile'),
+    ]
