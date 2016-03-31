84c26415c08e1446c5856ea0d23206ce429de331
minsoo <deadlylaid@gmail.com>
2016-03-31 05:09:58 +0000

    update:

--- photogram/users/models/user.py
+++ photogram/users/models/user.py
@@ -5,9 +5,11 @@ from django.db import models
 class User(AbstractUser):
     description = models.TextField()
 
+
     followee_set = models.ManyToManyField(
         "self",
         symmetrical=False,
-        related_name='follower_set',
         through='Follow',
+        related_name='follower_set',
+        through_fields=('follower', 'followee'),
     )
